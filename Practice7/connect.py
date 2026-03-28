314aafc433404786588c75eb9a8f91384900b861
zhekss12 <jeka11112007@gmail.com>
2026-03-28 12:10:55 +0500

    Add Practice7 - PhoneBook with PostgreSQL

---
+++ Practice7/connect.py
@@ -0,0 +1,11 @@
+import psycopg2
+from config import DB_CONFIG
+
+def get_connection():
+    conn = psycopg2.connect(
+        host=DB_CONFIG["host"],
+        database=DB_CONFIG["database"],
+        user=DB_CONFIG["user"],
+        password=DB_CONFIG["password"]
+    )
+    return conn
